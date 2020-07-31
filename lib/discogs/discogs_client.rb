fbc9b6d35bb282e2962cd1126b33cfb57a16155f
kowalix-pl <kowalix007@gmail.com>
2020-07-31 18:19:35 +0100

    set artist data search logic

--- lib/discogs/discogs_client.rb
+++ lib/discogs/discogs_client.rb
@@ -13,8 +13,18 @@ class DiscogsClient
     def search(phrase) 
        results = get_results(phrase)
        results.select {|result| result["type"] == "artist"}
+       search_artist_data
+       []
       #  release = results.find {|e| e["type"] == "release" && e["format"].include?("CD") && e["format"].include?("Album") }
       #  release
+    end 
+
+    def search_artist_data
+      json = @json_client.get '/artists/716297/releases'
+      releases = json["releases"].map do |release| 
+        "Release:#{release["title"]} \nLabel: #{release["label"]}\nYear: #{release["year"]}"
+      end 
+      puts releases
     end 
 
     def find_album(album_url)
