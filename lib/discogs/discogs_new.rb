0f0701e5d5b532e40371b92810c52d1c2b251228
kowalix-pl <kowalix007@gmail.com>
2020-07-31 12:11:05 +0100

    create user interface

---
+++ lib/discogs/discogs_new.rb
@@ -0,0 +1,18 @@
+class UserInterface
+ attr_reader :name 
+def initialize(name)
+ @message = "Welcome #{name} to our program. Please select one of two options to find more information about the artist or the album.
+ "
+end 
+
+def print_message
+ puts @message
+end 
+
+# Welcome message
+# Choose from 2 options
+# 1. Search by album name
+# 2. Search by artist name
+# 1
+# please enter the album name e
+end 
